98e4e8461289c108b38712da8abf58b6bc4f882c
PHONIEXz <paulexdavid15@gmail.com>
2026-09-23 17:31:04 +0100

    Pin deployed Sanity Studio app ID

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,5 +2,6 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {projectId: 'cxjysvlq', dataset: 'production'},
+  deployment: {appId: 'esqg5fnnr6jg2b67vtzfnczq'},
   server: {hostname: 'localhost', port: 3333},
 })
